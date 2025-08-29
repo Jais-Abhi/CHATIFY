60c5e0357884f161d7a7845337aa1ef35c58a788
Abhi <abhijaiswal7479@gmail.com>
2025-08-29 19:25:22 +0530

    added messageBox and makedresponsive

--- Frontend/src/components/MessageBox.jsx
+++ Frontend/src/components/MessageBox.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { FaArrowLeft } from "react-icons/fa";
+import { setSelectedUser } from '../Redux/Slices/userSlice';
 
 const MessageBox = () => {
+
+  const {selectedUser} = useSelector((state)=>state.user)
+  const dispatch = useDispatch()
   return (
-    <div>MessageBox</div>
+    <>
+    <div className={` lg:w-[70%] w-full ${selectedUser ? "block" : "hidden w-0"} h-screen  bg-blue-100 `} >
+      <div className=' flex items-center w-full h-[10%] bg-[rgb(168,79,192)] border-l-2 border-gray-300 rounded-b-[50px]' >
+        <div onClick={()=>dispatch(setSelectedUser(null))} className=' cursor-pointer text-[1.2rem] ml-4' >
+          <FaArrowLeft/>
+        </div>
+        <div className=' flex items-center w-full  ml-8'>
+          <img src={selectedUser?.profile?.path} className='h-[50px] w-[50px] rounded-full object-cover' alt="" />
+          <span className=' ml-4 text-2xl font-semibold text-gray-100'>
+            {selectedUser?.name}
+          </span>
+        </div>
+      </div>
+    </div>
+    {!selectedUser && 
+    <div className='lg:w-[70%] hidden lg:h-screen bg-blue-100 lg:flex flex-col justify-center items-center font-semibold' >
+      <div className='text-[2rem] '  >Welcome to Chatify,</div>
+      <div className='text-xl' >Start conversation now </div>
+    </div>
+    }
+
+    </>
+
+
   )
 }
 
--- Frontend/src/components/Sidebar.jsx
+++ Frontend/src/components/Sidebar.jsx
@@ -6,7 +6,7 @@ import { RiLogoutCircleLine } from "react-icons/ri";
 import { useNavigate } from 'react-router';
 import axios from 'axios';
 import { serverUrl } from '../main';
-import { setUserData } from '../Redux/Slices/userSlice';
+import { setSelectedUser, setUserData } from '../Redux/Slices/userSlice';
 
 
 
@@ -14,6 +14,7 @@ const Sidebar = () => {
     const navigate = useNavigate()
     const dispatch = useDispatch()
     const {otherUsers} = useSelector((state)=>state.user)
+    const {selectedUser} = useSelector((state)=>state.user)
     const {userData} = useSelector((state)=>state.user)
     const [searchBox , setSearchBox] = useState(false)
 
@@ -28,7 +29,7 @@ const Sidebar = () => {
 
 
   return (
-    <div className='w-[30%]  h-screen'>
+    <div className={`lg:w-[30%] ${selectedUser ? "hidden w-0" : "block"} w-full lg:block h-screen`}>
         <div className=' h-[25%] w-full bg-[rgb(168,79,192)] relative'  >
             <div className='text-white font-bold pl-4 text-[1.6rem]' >Chatify</div>
             <div className='text-white font-semibold pl-4 pt-2 text-[1.3rem]' > {userData.name} </div>
@@ -39,6 +40,9 @@ const Sidebar = () => {
                 <div onClick={()=>setSearchBox(true)} className=' h-full w-[45px] flex items-center justify-center absolute text-black text-[1.5rem] p-2 bg-white rounded-full cursor-pointer' >
                     <FaSearch/>
                 </div>
+
+                {/* SearchBox  */}
+
                 {searchBox && 
                 <div className='w-full h-full '>
                     <input type="text" className=' h-full pl-14 text-[1.2rem] p-2 rounded-full w-full' placeholder='Search chat' />
@@ -48,9 +52,12 @@ const Sidebar = () => {
                 </div>
                 }
             </div>
+
+            {/* Active users  */}
+
             { !searchBox && 
             <div className='ml-6 w-full h-full flex gap-4 items-center ' > 
-                <div className='h-[60px] w-[60px]   rounded-full ' >
+                <div className='h-[60px] w-[60px]  cursor-pointer rounded-full ' >
                     <img src={userData.profile.path} className=' h-[60px] w-[60px] object-cover rounded-full' alt="" />
                 </div>
                 <div className='h-[60px] w-[60px]  rounded-full' >
@@ -62,18 +69,19 @@ const Sidebar = () => {
             </div>
             }
             </div>
-            
 
+            {/* profile DP  */}
 
-            <div className='h-[60px] w-[60px] absolute right-5 top-6 rounded-full'>
+            <div onClick={()=>navigate("/profile")} className=' cursor-pointer h-[60px] w-[60px] absolute right-5 top-6 rounded-full'>
                 <img src={userData.profile.path} className='h-[60px] w-[60px] object-cover rounded-full' alt="" />
             </div>
         </div>
 
 
         <div className=' relative h-[75%] pt-2 border-collapse flex flex-col gap-2 bg-gray-100'>
+
             {otherUsers.map((user)=>
-                <div key={user._id} className=' rounded-l-full rounded-r-full  shadow-lg shadow-gray-500 hover:bg-[rgb(167,173,183)] ml-4 mr-4 p-1 flex items-center'>
+                <div key={user._id} onClick={()=> dispatch(setSelectedUser(user))} className='cursor-pointer rounded-l-full rounded-r-full  shadow-lg shadow-gray-500 hover:bg-[rgb(167,173,183)] ml-4 mr-4 p-1 flex items-center'>
                 <div className='h-[60px] w-[60px] rounded-full ' >
                     <img src={user.profile.path} className=' h-[60px] w-[60px] object-cover rounded-full' alt="" />
                 </div>
@@ -83,7 +91,10 @@ const Sidebar = () => {
             </div>
             )}
             
-            <div onClick={handleLogOut} className=' absolute bottom-4 left-4 text-3xl bg-[rgb(175,104,195)] rounded-full p-2 text-gray-100' >
+            {/* logout  */}
+
+
+            <div onClick={handleLogOut} className=' absolute bottom-4 left-4 text-3xl bg-[rgb(175,104,195)] rounded-full p-2 text-gray-100 cursor-pointer' >
                 <RiLogoutCircleLine />
             </div>
         </div>
