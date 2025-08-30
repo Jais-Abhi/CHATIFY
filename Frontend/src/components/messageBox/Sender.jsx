13712c0aa0bf699d97ae8ecf70bdf57157b9847c
Abhi <abhijaiswal7479@gmail.com>
2025-08-30 16:31:08 +0530

    Modified sender message

--- Frontend/src/components/messageBox/Sender.jsx
+++ Frontend/src/components/messageBox/Sender.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Sender = () => {
   return (
-    <div className=' h-fit break-words w-fit max-w-64 p-2 m-2 rounded-lg bg-purple-400'>
-        hellodfnnnnnnnnnnnnnsss Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, dolores esse placeat labore architecto quisquam corporis ad deserunt delectus accusamus numquam excepturi molestiae repellendus iusto aliquid praesentium nihil consequatur aperiam quis veniam earum, inventore ducimus! Deleniti ducimus numquam commodi modi consectetur, atque temporibus, voluptatibus officia quasi quia ipsam nemo nobis?
+    <div className=' h-fit break-words w-fit max-w-64 p-4 m-2 rounded-b-[30px] rounded-tr-[30px] bg-purple-400'>
+        hellodfnnnnnnnnnnnnnsss Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, dolores esse placeat labore architecto quisquam corporis ad deserunt delectus accusamus numquaum, inventore ducimus! Deleniti ducimus numquam commodi modi consectetur, atque temporibus, voluptatibus officia quasi quia ipsam nemo nobis?
     </div>
   )
 }
