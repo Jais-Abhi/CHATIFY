29bba727c7fbbef1f0684b34f7ae47be69df1f4a
Abhi <abhijaiswal7479@gmail.com>
2025-08-30 16:34:47 +0530

    added receiver message box

--- Frontend/src/components/messageBox/Receiver.jsx
+++ Frontend/src/components/messageBox/Receiver.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const Receiver = () => {
   return (
-    <div>Receiver</div>
-  )
+    <div className=' h-fit break-words w-fit max-w-64 p-4 m-2 rounded-b-[30px] rounded-tr-[30px] bg-purple-400'>
+        hellodfnnnnnnnnnnnnnsss Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, dolores esse placeat labore architecto quisquam corporis ad deserunt delectus accusamus numquaum, inventore ducimus! Deleniti ducimus numquam commodi modi consectetur, atque temporibus, voluptatibus officia quasi quia ipsam nemo nobis?
+    </div>  )
 }
 
 export default Receiver
