6cf3eb118b736d636b257bbfb8136fb3112ed765
Abhi <abhijaiswal7479@gmail.com>
2025-08-31 23:17:56 +0530

    socket cors setup

--- Backend/Socket/socket.js
+++ Backend/Socket/socket.js
@@ -5,3 +5,15 @@ import Server from "socket.io"
 
 const app = express()
 const server = http.createServer(app)
+
+const io = new Server(server,{
+    cors :{
+        origin : "http://localhost:5173",
+        Credential :true
+    }
+})
+
+
+
+
+export {server}
