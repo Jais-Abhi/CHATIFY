c1d9c94a260eabe65f3178c0a9273b7a53079f0b
Abhishek Jaiswal <abhijaiswal7479@gmail.com>
2025-09-03 00:21:05 +0530

    Update Receiver.jsx

--- Frontend/src/components/messageBox/Receiver.jsx
+++ Frontend/src/components/messageBox/Receiver.jsx
@@ -7,7 +7,7 @@ const Receiver = ({message}) => {
     })
   return (
     <div className='w-full flex justify-start ' >
-      <div ref={scroll} className=' h-fit break-words w-fit max-w-72 p-4 ml-8 m-4 rounded-b-[30px] rounded-tl-[30px] bg-purple-400'>
+      <div ref={scroll} className=' h-fit break-words w-fit max-w-60 p-4 ml-8 m-4 rounded-b-[30px] rounded-tl-[30px] bg-purple-400'>
         {message}
     </div>
     </div>
